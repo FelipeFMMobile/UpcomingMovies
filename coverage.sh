5d3356f8bb15d3f2e0a9ce753b98e5fb18d29b46
Felipe FMMobile <fmmobile@live.com>
2019-09-01 15:48:00 -0300

    contador de linhas de codigo

--- coverage.sh
+++ coverage.sh
@@ -2,12 +2,33 @@
 function convert_file {
   local xccovarchive_file="$1"
   local file_name="$2"
+  local not_covereged="([0-9][0-9]*): 0.*"
+  local covereged="([0-9][0-9]*): [1-9].*"
   echo "  <file path=\"$file_name\">"
-  xcrun xccov view --file "$file_name" "$xccovarchive_file" | \
-    sed -n '
-    s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
-    s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
-    '
+  let total=0
+  TEMPFILE="/tmp/$$.tmp"
+  echo 0 > $TEMPFILE
+  xcrun xccov view --file "$file_name" "$xccovarchive_file" | 
+  while read -r line_cover; do
+    #echo "$line_cover"
+      if [[ "$line_cover" =~ $covereged ]]; then 
+        #echo "Covereged"
+        total=$[$total +1]
+      fi 
+      if [[ "$line_cover" =~ $not_covereged ]]; then 
+        #echo "Not Coverage "
+        total=$[$total +1]
+      fi
+      echo $total > $TEMPFILE
+  done
+  result=$[$(cat $TEMPFILE)]
+  echo " Total lines: $result"
+  unlink $TEMPFILE
+    # sed -n '
+    # s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
+    # s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
+    # '
+  
   echo '  </file>'
 }
 
@@ -15,14 +36,14 @@ function xccov_to_generic {
   echo '<coverage version="1">'
   for xccovarchive_file in "$@"; do
     xcrun xccov view --file-list "$xccovarchive_file" | while read -r file_name; do
-    local pattern="(ViewModel)(.swift)"
+    local pattern="(.swift)"
     # if [[ $file_name =~ $pattern ]]; then
     #     echo "Valid file"
     # else
     #     echo "Invalid file $file_name"
     # fi
     if [[ $file_name =~ $pattern ]]; then
-        echo "$file_name"
+        #echo "$file_name"
         convert_file "$xccovarchive_file" "$file_name"
     fi
     done
