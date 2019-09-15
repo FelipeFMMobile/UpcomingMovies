7510e5c7e921cb3e9801be8d94279a8ea8648ffb
Felipe FMMobile <fmmobile@live.com>
2019-09-15 18:02:35 -0300

    Finalização do script que verifica cobertura de testes em arquivos com determinado pattern regex.

--- coverage_Copy.sh
+++ coverage_Copy.sh
@@ -1,11 +1,10 @@
 
-patternFiles="(ViewModel)(.swift|.docx|.pdf)"
-
-function coverage_file {
+function convert_file {
   local xccovarchive_file="$1"
   local file_name="$2"
   local not_covereged="([0-9][0-9]*): 0.*"
   local covereged="([0-9][0-9]*): [1-9].*"
+  #echo "  <file path=\"$file_name\">"
   let totalLines=0
   let coveraged=0
   let cover=0
@@ -13,50 +12,51 @@ function coverage_file {
   echo 0 > $TEMPFILE
   xcrun xccov view --file "$file_name" "$xccovarchive_file" | 
   while read -r line_cover; do
+    #echo "$line_cover"
       if [[ "$line_cover" =~ $covereged ]]; then 
+        #echo "Covereged"
         totalLines=$[$totalLines +1]
         coverage=$[$coverage +1]
       fi 
       if [[ "$line_cover" =~ $not_covereged ]]; then 
+        #echo "Not Coverage "
         totalLines=$[$totalLines +1]
       fi
       if [[ $totalLines > 0 ]]; then
+      #echo $coverage
+      #echo $total
         cover=$(echo "$[$coverage]/$[$totalLines]" | bc -l)
       fi
       echo $cover > $TEMPFILE
   done
   result=$(cat $TEMPFILE)
-  #echo " Coveraged for $file_name: $result"
+  echo " Coveraged for $file_name: $result"
   unlink $TEMPFILE
+    # sed -n '
+    # s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
+    # s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
+    # '
+  
+  #echo '  </file>'
 }
 
-function getCoverage {
-  local testPath="Build/Logs/Test/*.xcresult/*_Test/*.xccovarchive"
-  local fileCount=0
-  local sumCoverage=0
-  local averageCoverage=0
-  local result
-  TEMPFILE="/tmp/$$.tmp"
-  echo 0 > $TEMPFILE
-  for xccovarchive_file in $testPath; do
+function xccov_to_generic {
+  #echo '<coverage version="1">'
+  for xccovarchive_file in "$@"; do
     xcrun xccov view --file-list "$xccovarchive_file" | while read -r file_name; do
-    local pattern="$@"
-    if [ "$@" ]; then
-      if [[ $file_name =~ $pattern ]]; then
-          coverage_file "$xccovarchive_file" "$file_name"
-          #echo $result
-          fileCount=$(echo $fileCount +1 | bc -l)
-          sumCoverage=$(echo "$sumCoverage+$result" | bc -l)
-          #echo "$sumCoverage / $fileCount"
-          averageCoverage=$(echo "$sumCoverage/$fileCount" | bc -l)
-          echo $averageCoverage > $TEMPFILE
-      fi
+    local pattern="(.swift)"
+    # if [[ $file_name =~ $pattern ]]; then
+    #     echo "Valid file"
+    # else
+    #     echo "Invalid file $file_name"
+    # fi
+    if [[ $file_name =~ $pattern ]]; then
+        #echo "$file_name"
+        convert_file "$xccovarchive_file" "$file_name"
     fi
     done
   done
-  averageCoverage=$(cat $TEMPFILE)
-  percent=$(echo "scale=0; $averageCoverage*100" | bc | xargs printf "%.1f")
-  echo "Total coverage: $percent% for '$@' pattern files"
+  #echo '</coverage>'
 }
 
 #Build
@@ -69,4 +69,4 @@ function getCoverage {
 
 #xccov_to_generic "$@" > test.xml
 
-getCoverage $patternFiles
+xccov_to_generic Build/Logs/Test/*.xcresult/*_Test/*.xccovarchive
